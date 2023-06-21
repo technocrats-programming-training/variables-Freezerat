42e8a09a94cd3d3ffd92bdebd2c7a80a3530594e
Freezerat <23920355-Freezerat@users.noreply.replit.com>
2023-06-21 00:42:50 +0000

    completed assignment

--- Main.java
+++ Main.java
@@ -1,16 +1,26 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         // DO NOT CHANGE 
-        String firstName = args[0];
-        String lastName = args[1];
+        final String firstName = args[0];
+        final String lastName = args[1];
         int age = 0;
         try {
             age = Integer.parseInt(args[2]);
-        } catch (NumberFormatException ex) {
+        } catch (final NumberFormatException ex) {
             ex.printStackTrace();
         }
+         boolean areNamesSame;
+         boolean age3;
+         int age2;
+         boolean isAgeEven;
+         areNamesSame = firstName == lastName;
+         age2 = age % 2; 
+           isAgeEven = 0 == age2;
+         age3 = age > 0; 
+        System.out.print("Hello, I am " +firstName+ " " +lastName+ "and I am " +age+ " " + "years old, is my age even? " +isAgeEven+ "is my age greater than zero? " +age3+ " are my first and last names the same? " +areNamesSame );
         
-
+       
+         
         // Print out the following string: "Hello, I am <firstName> <lastName>, and I am <age> years old"
 
         // Print out whether age is even
